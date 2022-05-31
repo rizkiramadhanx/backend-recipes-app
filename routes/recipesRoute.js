5e36474a81a35aa33290edf352fe7aa640183c7b
rizkiramadhanx <rizkijitu77@gmail.com>
2022-05-31 17:02:02 +0700

    Edit Recipes

--- routes/recipesRoute.js
+++ routes/recipesRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
 import {
   addRecipe,
+  editRecipes,
   favoriteRecipesByUsername,
   getAllRecipes,
+  getAllRecipesByCategory,
   myFavoriteRecipes,
+  myRecipes,
   searchRecipes,
 } from "../controllers/recipesController.js";
 import { DataSpecificById } from "../controllers/userController.js";
@@ -11,11 +14,14 @@ import { verifyToken } from "../middlewares/authentication.js";
 
 const router = express.Router();
 
-router.route("/all").get(searchRecipes);
+// router.route("/all").get(searchRecipes);
 router.route("/all").get(getAllRecipes);
 router.route("/add").post([verifyToken], addRecipe);
 router.route("/favorite").get([verifyToken], myFavoriteRecipes);
-router.route("/favorite").get([verifyToken], myFavoriteRecipes);
 router.route("/favorite/:username").get(favoriteRecipesByUsername);
+router.route("/category/:category").get(getAllRecipesByCategory);
+router.route("/my").get([verifyToken], myRecipes);
+router.route("/my/:idRecipe").put([verifyToken], editRecipes);
+router.route("/my").get([verifyToken], myRecipes);
 
 export default router;
